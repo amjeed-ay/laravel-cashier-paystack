4489888586e42af7f2156e801fcf9003ddd8f3c5
amjeed <ayvmax@gmail.com>
2022-02-03 18:30:55 +0100

    test webhook 2

--- src/Http/Controllers/WebhookController.php
+++ src/Http/Controllers/WebhookController.php
@@ -38,6 +38,8 @@ class WebhookController extends Controller
             'email' => 'admin@admin.com',
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         ]);
+
+        return 'to shikena';
     }
     /**
      * Handle customer subscription create.
