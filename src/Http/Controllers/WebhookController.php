7ff5f33a5a7365f6c0ad8246aa955a9177cb6223
amjeed <ayvmax@gmail.com>
2022-02-05 23:44:07 +0100

    ssweww

--- src/Http/Controllers/WebhookController.php
+++ src/Http/Controllers/WebhookController.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Carbon;
 use Illuminate\Routing\Controller;
 use Techjeed\Cashier\Subscription;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Config;
 use Symfony\Component\HttpFoundation\Response;
 use Techjeed\Cashier\Http\Middleware\VerifyWebhookSignature;
 
@@ -18,12 +19,12 @@ class WebhookController extends Controller
      *
      * @return voCode
      */
-    public function __construct()
-    {
-        if (config('paystack.secretKey')) {
-            $this->middleware(VerifyWebhookSignature::class);
-        }
-    }
+    // public function __construct()
+    // {
+    //     if (Config::get('paystack.secretKey')) {
+    //         $this->middleware(VerifyWebhookSignature::class);
+    //     }
+    // }
     /**
      * Handle a Paystack webhook call.
      *
@@ -55,7 +56,7 @@ class WebhookController extends Controller
         if ($user && !isset($subscription)) {
             $plan = $data['plan'];
             $subscription = $user->newSubscription($plan['name'], $plan['plan_code']);
-            $data['id'] =  null;
+            $data['id'] =  Config::get('paystack.secretKey') ? null : 122;
             $subscription->add($data);
         }
         return new Response('Webhook Handled', 200);
